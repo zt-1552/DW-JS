765ba4179e2a0eab8a505156bcc010001c15c2d8
Yurii Kovalchuk <zt1552@gmail.com>
2019-06-05 22:18:46 +0300

    Counter working!

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,17 +1,30 @@
 ;(function($, undefined) {
 
   $('#btn-top-red').on('click', function (){
-    $('#btn-top-red').hide();
-    $('#btn-top-grey').show();
-    counter5second();
-  })
+    $('#btn-top-red').hide(200);
+    $('#btn-top-grey').show(200);
+    setTimeout(startCountUp, 1000);
+})
 
-function counter5second() {
-  var count = 5;
-  for (var i = 0; i <= count; i++) {
-    $('#btn-top-grey').html(i + 'секунд').delay(5000);
-    console.log(i + 'секунд');
+
+  var countdown = $('#btn-top-grey'),
+      text = 'секунд',
+      timer;
+  function startCountUp(){
+      var start = 0;
+      timer = setInterval(function(){
+          if (start == 0 || start == 5) {
+            countdown.text(start++ + ' ' + text);
+          } else if (start == 1) {
+            countdown.text(start++ + ' ' + 'секунда');
+          }else {
+            countdown.text(start++ + ' ' + 'секунды');
+          }
+          if(start > 6) {
+              clearInterval(timer);
+              countdown.text('Готово');
+          }
+      },1000);
   }
-}
 
 })(jQuery);
